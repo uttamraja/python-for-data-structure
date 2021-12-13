f0ebb3d7abf7d427f0b5bc3130ab6e4131e554b5
Uttam Rajbanshi <uttamraja41@gmail.com>
2021-12-13 08:35:52 +0545

    stack program using python

---
+++ stack.py
@@ -0,0 +1,26 @@
+#creating empty list
+
+stack = []
+
+#adding  element in the empty list
+
+
+stack.append(1)
+stack.append(2)
+stack.append(3)
+stack.append(4)
+stack.append(5)
+stack.append(20)
+stack.append(9)
+stack.append(12)
+#printing the list
+
+print(stack)
+
+#sorting the element in the list
+stack.sort()
+
+
+#printing the list after sorting
+
+print(stack)
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,12 @@
+#creating empty list
+
+stack = []
+
+#adding  element in the empty list
+
+
+stack.append(1)
+
+#printing the list
+
+print(stack)
